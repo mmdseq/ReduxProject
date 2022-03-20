6cd6f8ff3a2bf581012d6ec5bae9b05dd52cd86a
mmdseq <mmdseq@gmail.com>
2022-03-21 00:58:10 +0330

    Redux toolkit added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,140 @@
-import logo from './logo.svg';
-import './App.css';
+//------------- side effects inside component its ok i think --------
+// import Cart from "./components/Cart/Cart";
+// import Layout from "./components/Layout/Layout";
+// import Products from "./components/Shop/Products";
+// import { useEffect, Fragment } from "react";
+// import Notification from "./components/UI/Notification";
+
+// import { useSelector, useDispatch } from "react-redux";
+// import { uiActions } from "./store/ui-slice";
+// import {sendCartData} from './store/cart-slice'
+
+// let isInitial = true;
+
+// function App() {
+//   const dispatch = useDispatch();
+//   const showCart = useSelector((state) => state.ui.cartIsVisible);
+//   const cart = useSelector((state) => state.cart);
+//   const notification = useSelector((state) => state.ui.notification);
+
+//   useEffect(() => {
+//     const sendCartData = async () => {
+//       dispatch(
+//         uiActions.showNotification({
+//           status: "pending",
+//           title: "Sendding...",
+//           message: "Sending cart data!",
+//         })
+//       );
+//       const response = await fetch(
+//         `https://react-http-b780e-default-rtdb.firebaseio.com/cart.json`,
+//         {
+//           method: "PUT",
+//           body: JSON.stringify(cart),
+//         }
+//       );
+
+//       if (!response.ok) {
+//         // throw new Error("Sending cart data failed.");
+//       }
+
+//       dispatch(
+//         uiActions.showNotification({
+//           status: "success",
+//           title: "Success!",
+//           message: "Sent cart data successfully!",
+//         })
+//       );
+
+//       // const responseData = await response.json();
+//     };
+
+//   if (isInitial) {
+//     isInitial = false;
+//     return;
+//   }
+
+//     sendCartData().catch((error) => {
+//       dispatch(
+//         uiActions.showNotification({
+//           status: "error",
+//           title: "Error!",
+//           message: "Sending cart data failed!",
+//         })
+//       );
+//     });
+//   }, [cart, dispatch]);
+//   return (
+//     <Fragment>
+//       {notification && (
+//         <Notification
+//           status={notification.status}
+//           title={notification.title}
+//           message={notification.message}
+//         />
+//       )}
+//       <Layout>
+//         {showCart && <Cart />}
+//         <Products />
+//       </Layout>
+//     </Fragment>
+//   );
+// }
+
+// export default App;
+
+//------------------------ side effects with action creator -- thunk---------------
+
+//------------- side effects inside component its ok i think --------
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useEffect, Fragment } from "react";
+import Notification from "./components/UI/Notification";
+
+import { useSelector, useDispatch } from "react-redux";
+// import { uiActions } from "./store/ui-slice";
+import { fetchCartData, sendCartData } from "./store/cart-actions";
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    if(cart.changed){
+      dispatch(sendCartData(cart));
+
+    }
+
+
+  }, [cart, dispatch]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
